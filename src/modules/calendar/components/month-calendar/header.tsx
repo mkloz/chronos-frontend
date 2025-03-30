90f2584c6c332d81a501262f0832dede9c080b78
Mykhailo Kloz <micha21cloz@gmail.com>
2025-03-30 10:49:23 +0100

    fix: month day

--- src/modules/calendar/components/month-calendar/header.tsx
+++ src/modules/calendar/components/month-calendar/header.tsx
@@ -11,7 +11,7 @@ export const MonthCalendarHeader = () => {
             key={i}
             className={cn(
               'flex flex-row items-center justify-center font-semibold gap-2 text-accent-foreground bg-accent text-xl leading-none grow text-center py-3 rounded-[1.25rem] w-24 @max-lg:text-sm align-center ',
-              (i === 5 || i == 6) && 'border-3 border-neutral-900'
+              (i === 0 || i == 6) && 'border-3 border-neutral-900'
             )}>
             <span>{day}</span>
           </h4>
